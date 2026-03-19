6e4ad6c98b80e349a03ab26db17b05c1296636e1
Eddie Wiik Nilsson <eddie.wiiknilsson@gmail.com>
2026-03-19 18:18:08 +0100

    Beginning of RenderMessages dynamically from DB

---
+++ javascript/RenderMessages.js
@@ -0,0 +1,15 @@
+export const RenderMessages = async(usersObject, messagesObject) => {
+
+    for (const key in usersObject) {
+        
+        const element = usersObject[key];
+        console.log(key)
+        console.log(element)
+        
+        for (const element of messagesObject) {
+            
+        }
+
+    }
+
+}
--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,22 @@
 import { reference, db } from "./firebase.js"
+import { RenderMessages } from "./RenderMessages.js"
 import { push, onValue, remove, ref } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-database.js";
 import { RenderMessageBox } from "./RenderMessageBox.js";
 
 onValue(reference, snapshot => {
     const data = snapshot.val();
-    console.log(data);
+
+    const usersObject = data.users;
+    const messagesObject = data.messages;
+
+    RenderMessages(usersObject, messagesObject)
+
+
+    // console.log(data);
 }, error => {
     console.error("Firebase read failed:", error);
     alert("Kunde inte hämta data från databasen.")
 });
+
 
 RenderMessageBox("Eddie", "I hate my life");
